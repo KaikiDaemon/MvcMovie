cf9eb34a058267be9f2106b2c7af12434d5d9958
KaikiDaemon <Hibokukami@gmail.com>
2025-09-05 21:04:14 -0400

    Stuck part 6

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,6 +6,8 @@ namespace MvcMovie.Models
     {
         public int              Id { get; set; }
         public string?          Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime         ReleaseDate { get; set; }
         public string?          Genre { get; set; }
