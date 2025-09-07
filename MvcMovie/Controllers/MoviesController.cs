8b80a68241d1efb01cfdbe9def225cf25a7d85d5
KaikiDaemon <Hibokukami@gmail.com>
2025-09-06 21:25:32 -0400

    search broke

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,10 +19,22 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
-        // GET: Movies
-        public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index(string id)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie != null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie' is null.");
+            }
+       
+            var movies = from m in _context.Movie select m;
+       
+            if (!String.IsNullOrEmpty(id))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(id.ToUpper()));
+            }
+       
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
